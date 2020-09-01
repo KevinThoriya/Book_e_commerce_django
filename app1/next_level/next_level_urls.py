99df0dc32ded759261a8b23882074b062773c5c3
KevinThoriya <kevinthoriya0074@gmail.com>
2020-09-01 23:41:37 +0530

    -- added product_view

--- app1/next_level/next_level_urls.py
+++ app1/next_level/next_level_urls.py
@@ -3,5 +3,6 @@ from . import next_level_views as views
 
 base_url = 'next_level'
 urlpatterns = [
-    path('next_level/home/',views.index,name="home_n")
+    path('next_level/home/',views.index,name="home_n"),
+    path('next_level/product/',views.product_detail,name="product_detail")
 ]
--- app1/next_level/next_level_views.py
+++ app1/next_level/next_level_views.py
@@ -3,3 +3,6 @@ from app1.models import Contact, Register, Book, Cart, Wishlist, Order_details, 
 
 def index(request):
     return render(request,'app1/next_level/index.html')
+
+def product_detail(request):
+    return render(request,'app1/next_level/product_detail_page.html')
